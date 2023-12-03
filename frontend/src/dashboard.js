4c4957f4968ab1acdb5e0b182b66e4a2775d1ce1
Christy Xie <cxie62@uwo.ca>
2023-12-03 18:24:24 -0500

    key metrics start

--- frontend/src/dashboard.js
+++ frontend/src/dashboard.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './dashboard.css'; // Importing the CSS file for styling
 import './dropdown.css';
 import Dropdown from './dropdown.js';
+import KeyMetrics from './keyMetrics.js';
 
 const Dashboard = () => {
   return (
@@ -11,6 +12,7 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       {/* You can add more components or content here */}
         <Dropdown />
+        <KeyMetrics />
     </div>
   );
 };
