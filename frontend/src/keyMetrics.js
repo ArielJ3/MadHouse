f935024f97012bb6a2182a981e5e3577fff32837
Christy Xie <cxie62@uwo.ca>
2023-12-03 19:42:52 -0500

    fixed key metrics layout

--- frontend/src/keyMetrics.js
+++ frontend/src/keyMetrics.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './keyMetrics.css';
 
 const  KeyMetrics: React.VFC = () => {
@@ -6,82 +6,40 @@ const  KeyMetrics: React.VFC = () => {
     <div className="key-metrics">
       <div className="metrics-card">
         <div className="metrics-content">
-          <div className="metrics-text">
-            <p className="text-1">Population</p>
-          </div>
+            <p className="metrics-title">Population</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p className="text-2">721K</p>
-          </div>
-          <div className="icon-text">
-            <div className="text">
-              <p className="text-3">+11.01%</p>
-            </div>
-            <div className="icon-set">
-              <img src="" />
-            </div>
-          </div>
+        <div className="metrics-content">
+            <p className="metrics-number">721K</p>
+              <p className="metrics-change">+11.01%</p>
         </div>
       </div>
-      <div className="card">
-        <div className="content">
-          <div className="text">
-            <p className="text-5">Number of Houses</p>
-          </div>
+
+      <div className="metrics-card">
+        <div className="metrics-content">
+            <p className="metrics-title">Number of Houses</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p className="text-6">367K</p>
-          </div>
-          <div className="icon-text">
-            <div className="text">
-              <p className="text-7">-0.03%</p>
-            </div>
-            <div className="icon-set">
-              <img src="" />
-            </div>
-          </div>
+        <div className="metrics-content">
+            <p className="metrics-number">367K</p>
+              <p className="metrics-change">-0.03%</p>
         </div>
       </div>
-      <div className="card">
-        <div className="content">
-          <div className="text">
-            <p className="text-9">some metric</p>
-          </div>
+
+      <div className="metrics-card">
+        <div className="metrics-content">
+            <p className="metrics-title">some metric</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p className="text-1-0">1,156</p>
-          </div>
-          <div className="icon-text">
-            <div className="text">
-              <p className="text-1-1">+15.03%</p>
-            </div>
-            <div className="icon-set">
-              <img src="" />
-            </div>
-          </div>
+        <div className="metrics-content">
+            <p className="metrics-number">1,156</p>
+              <p className="metrics-change">+15.03%</p>
         </div>
       </div>
-      <div className="card">
-        <div className="content">
-          <div className="text">
-            <p className="text-1-3">Some Metric 2</p>
-          </div>
+      <div className="metrics-card">
+        <div className="metrics-content">
+            <p className="metrics-title">Some Metric 2</p>
         </div>
-        <div className="content">
-          <div className="text">
-            <p className="text-1-4">239K</p>
-          </div>
-          <div className="icon-text">
-            <div className="text">
-              <p className="text-1-5">+6.08%</p>
-            </div>
-            <div className="icon-set">
-              <img src="" />
-            </div>
-          </div>
+        <div className="metrics-content">
+            <p className="metrics-number">239K</p>
+              <p className="metrics-change">+6.08%</p>
         </div>
       </div>
     </div>
